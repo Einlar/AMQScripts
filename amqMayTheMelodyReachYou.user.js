19fe24696d4fbde7f2577dd398f42eda29e35672
Francesco Manzali <manzali.francesco97@gmail.com>
2024-10-20 23:05:22 +0200

    Support mac for shortcut

--- amqMayTheMelodyReachYou.user.js
+++ amqMayTheMelodyReachYou.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ May the Melody Reach You
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.45
 // @description  Show the Song/Artist matches for the current song when playing in a S/A room with the Ensemble Song Artist script enabled. Works even while spectating!
 // @author       Einlar
 // @match        https://animemusicquiz.com/*
@@ -136,7 +136,7 @@ const setup = () => {
   };
 
   document.addEventListener("keydown", (e) => {
-    if (e.altKey && e.key === "b") {
+    if ((e.altKey || e.metaKey) && e.key === "b") {
       active = ws === null ? true : !active;
 
       if (!active) {
